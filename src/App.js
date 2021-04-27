4bfbb33a09274dc9462784bf3d13d0d5e90a5074
Gautam <pbg2402@gmail.com>
2021-04-27 12:25:23 +0530

    Added: new intro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import NewIntro from './Components/NewIntro/Intro'
 import Intro from "./Components/Intro/Intro";
 import About from "./Components/About/About";
 import What from "./Components/What/What";
@@ -15,6 +16,7 @@ function App() {
     <div className="app">
       {/* <h1>Pyramid Developers</h1>
       <h2>This is a trial</h2> */}
+	<NewIntro />
       <Intro />
       <About />
       <What />
