58c876c31db9015e77a127eb4c702835f36792cf
NihalS <nihalshettybk@gmail.com>
2021-04-30 00:05:22 +0530

    Our Team Component Members Update

--- src/Components/OurTeam/OurTeam.js
+++ src/Components/OurTeam/OurTeam.js
@@ -28,16 +28,18 @@ const OurTeam = () => {
                     </div>
                     <div className="col3">
                        <Member image={dhruv} name="Dhruv Shetty" role="Business Analyst & Core Developer"/>
-                       <Member image={adithya} name="Adithya Aravind" role="Core Developer"/>
-                       <Member image={pratheek} name="Pratheek SB" role="Core Developer"/>
+                       <Member image={samarth} name="Samarth Kashyap" role="Business & Strategy"/>
                        <Member image={ayush} name="Ayush Kiran" role="Lead Designer"/>
+                       <Member image={ankit} name="Ankit Mallya" role="Legal"/>
+                       {/* <Member image={adithya} name="Adithya Aravind" role="Core Developer"/>
+                       <Member image={pratheek} name="Pratheek SB" role="Core Developer"/> */}
                     </div>
                     <div className="col4">
                         <div className="greyRectangle no-border curve-rectangle-left"></div>
                         <div className="greyRectangle no-border"></div>
                     </div>
                 </div>
-                <div className="bottom__lower">
+                {/* <div className="bottom__lower">
                     <div className="push">
                         <div className="darkRectangle"></div>
                     </div>
@@ -60,7 +62,7 @@ const OurTeam = () => {
                         <div className="greyRectangle no-border curve-rectangle-left"></div>
                         <div className="greyRectangle no-border curve-rectangle-right"></div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
