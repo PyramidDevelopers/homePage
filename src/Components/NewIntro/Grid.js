6dbfcc0d796be909d5bb0a4082ac4e98086ba9b0
NihalS <nihalshettybk@gmail.com>
2021-06-20 14:11:23 +0530

    Footer Component Static Bug Fix

--- src/Components/NewIntro/Grid.js
+++ src/Components/NewIntro/Grid.js
@@ -32,8 +32,8 @@ function Grid() {
     const [{ globalCount }, setGlobalCounter] = useStateValue()
     const [counter, setCounter] = useState(1)
     const [color, setColor] = useState(THEME[globalCount])
-    const [row, setRow] = useState(150);
-    const [margin, setMargin] = useState([20,20]);
+    // const [row, setRow] = useState(150);
+    // const [margin, setMargin] = useState([20,20]);
     
 
     const changeLayout = () => {
@@ -50,25 +50,25 @@ function Grid() {
         setCounter((counter + 1) % 8)
         setColor(THEME[counter])
     }
-    useEffect(() => {
-        let width = window.innerWidth;
-        if(width > 1024){
-            setRow(150)
-            setMargin([20,20])
-        }
-        else if(width > 768){
-            setRow(100)
-            setMargin([10,10])
-        }
-        else if(width > 480){
-            setRow(47)
-            setMargin([7,7])
-        }
-        else{
-            setRow(30)
-            setMargin([5,5])
-        }
-    }, [window.innerWidth])
+    // useEffect(() => {
+    //     let width = window.innerWidth;
+    //     if(width > 1024){
+    //         setRow(150)
+    //         setMargin([20,20])
+    //     }
+    //     else if(width > 768){
+    //         setRow(100)
+    //         setMargin([10,10])
+    //     }
+    //     else if(width > 480){
+    //         setRow(47)
+    //         setMargin([7,7])
+    //     }
+    //     else{
+    //         setRow(30)
+    //         setMargin([5,5])
+    //     }
+    // }, [window.innerWidth])
 
     return (
         <div className="Grid">
@@ -76,11 +76,11 @@ function Grid() {
                 useCSSTransforms={true}
                 className="layout"
                 layouts={layouts[color]}
-                rowHeight={row}
+                rowHeight={150}
                 isDraggable={false}
                 isResizable={false}
                 compactType={'vertical'}
-                margin={margin}
+                margin={[20,20]}
                 measureBeforeMount={false}
                 breakpoints={{ lg: 1024, md: 768, sm: 480, xs: 0 }}
                 cols={{ lg: 84, md: 42, sm: 56, xs: 28}}
