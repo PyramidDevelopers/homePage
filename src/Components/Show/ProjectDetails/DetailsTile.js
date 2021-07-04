1b02314c968faad288533b9f76807dc58e9ba3cb
Raashi Agarwal <raashiaggarwal21@gmail.com>
2021-07-05 03:43:46 +0530

    Modified CSS part

--- src/Components/Show/ProjectDetails/DetailsTile.js
+++ src/Components/Show/ProjectDetails/DetailsTile.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { DetailedInformation } from '../Details'
-import CloseIcon from '@material-ui/icons/Close';
 import './DetailsTile.css'
 
 function DetailsTile({ index, onClose }) {
@@ -8,43 +7,39 @@ function DetailsTile({ index, onClose }) {
         <div className="project-details-container">
             <div className="project-details-header">
                 <div className="details-tile-title">{DetailedInformation[index].title}</div>
-                <button className="color-rect-close-icon" onClick={onClose}>
-                    <CloseIcon />
+                <button className="color-rect-close" onClick={onClose}>
+                    <div className="close-tile">x</div>
                 </button>
             </div>
             <div className="status-and-client-section">
                 <div className="status-section">
-                    <div className="color-rect-status-client">STATUS</div>
-                    <div className="status-client-text">{DetailedInformation[index].status}</div>
+                    <div className="color-rect-for-label">STATUS</div>
+                    <div className="details-text">{DetailedInformation[index].status}</div>
                 </div>
                 <div className="client-section">
-                    <div className="color-rect-status-client">CLIENT</div>
-                    <div className="status-client-text">{DetailedInformation[index].client}</div>
+                    <div className="color-rect-for-label">CLIENT</div>
+                    <div className="details-text">{DetailedInformation[index].client}</div>
                 </div>
             </div>
             <div className="info-scope-links-section">
                 <div className="color-rect-for-label">INFO</div>
-                <div className="details-text-container">
-                    <p>{DetailedInformation[index].info}</p>
-                </div>
+                <div className="details-text">{DetailedInformation[index].info}</div>
             </div>
             <div className="info-scope-links-section">
                 <div className="color-rect-for-label">SCOPE</div>
-                <div className="details-text-container">
-                    <p>{DetailedInformation[index].scope}</p>
-                </div>
+                <div className="details-text">{DetailedInformation[index].scope}</div>
             </div>
             <div className="info-scope-links-section">
                 <div className="color-rect-for-label">LINKS</div>
-                <div className="details-text-container">
-                    {DetailedInformation[index].linksTrue && DetailedInformation[index].links.map((link, index)=>(
+                <div className="details-text">
+                    {DetailedInformation[index].linksTrue && DetailedInformation[index].links.map((link, index) => (
                         <div key={index}>
-                            {!link.inProgress && <><a href={link.link} target="_blank">{link.title}</a><br/></>}
-                            {link.inProgress && <>{link.title}: In Progress<br/></>}
+                            {!link.inProgress && <><a href={link.link} target="_blank">{link.title}</a><br /></>}
+                            {link.inProgress && <>{link.title}: In Progress<br /></>}
                         </div>
                     ))
                     }
-                    {!DetailedInformation[index].linksTrue &&<>{DetailedInformation[index].linkWriteUp}<br/></>}
+                    {!DetailedInformation[index].linksTrue && <>{DetailedInformation[index].linkWriteUp}<br /></>}
                 </div>
             </div>
         </div>
