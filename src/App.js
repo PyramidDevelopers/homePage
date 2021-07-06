ac6a28b26bdfb414dacfc7ec99ce828f980b520a
imran mirza <imranmirza070901@gmail.com>
2021-07-06 23:44:05 +0530

    Added testimonials

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import OurTeam from './Components/OurTeam/OurTeam'
 import ReachUs from './Components/ReachUs/ReachUs'
 import Footer from './Components/Footer/Footer'
 import Testimonial from './Components/Testimonials/Testimonials'
-import TestimonialWrapper from './Components/Testimonials/TestimonialsWrapper'
-
 function App() {
     // useEffect(() => {
     //     // browser.tabs.setZoom()
@@ -26,12 +24,7 @@ function App() {
             <What />
             <Show />
             <Choose />
-            <TestimonialWrapper/>
-            <Testimonial
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-                clientName="Client Name"
-                companyName="Company Name"
-            />
+            <Testimonial/>
             <OurTeam />
             <ReachUs />
             <Footer />
