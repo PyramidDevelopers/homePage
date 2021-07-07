60a6c2073e8777e38c4dbe36f99098f3b3d72e98
NihalS <nihalshettybk@gmail.com>
2021-07-07 16:58:51 +0530

    OurTeam Component Updated With New Members

--- src/Components/OurTeam/OurTeam.js
+++ src/Components/OurTeam/OurTeam.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './OurTeam.css'
 import dhruv from '../../assets/dhruv-shetty.png'
-// import adithya from '../../assets/adithya-aravind.png';
-// import pratheek from '../../assets/pratheek-sb.png';
-import ayush from '../../assets/ayush-kiran.png'
 import ankit from '../../assets/ankit-mallya.png'
-// import rishi from '../../assets/rishi-pachipulusu.png';
+import ayush from '../../assets/ayush-kiran-new.png'
 import samarth from '../../assets/samarth-kashyap.png'
+import pratheek from '../../assets/pratheek-sb.png';
+import stuti from '../../assets/stuti-mohanty.png';
+import srujit from '../../assets/srujit-naina.png';
+import ritesh from '../../assets/ritesh-sapata.png';
+// import ayush from '../../assets/ayush-kiran.png'
+// import adithya from '../../assets/adithya-aravind.png';
+// import rishi from '../../assets/rishi-pachipulusu.png';
 import Member from '../Member/Member'
 
 const OurTeam = () => {
@@ -18,15 +22,70 @@ const OurTeam = () => {
                 </div>
             </div>
             <div className="ourTeam__bottom">
-                <div className="bottom__upper">
-                    <div className="col1 mobile__hidden">
-                        <div className="darkRectangle"></div>
-                        <div className="greyRectangle"></div>
+                <div className="bottom__col1">
+                    <div className="darkRectangle"></div>
+                    <div className="greyRectangle"></div>
+                    <div className="greyRectangle"></div>
+                    <div className="darkRectangle"></div>
+                </div>
+                <div className="bottom__col2">
+                    <div className="greyLongRectangle"></div>
+                    <div className="darkRectangle"></div>
+                    <div className="greyRectangle"></div>
+                </div>
+                <div className="bottom__col3">
+                    <div className="col3__upper">
+                        <Member
+                            image={dhruv}
+                            name="Dhruv Shetty"
+                            role="Business Analyst & Core Developer"
+                        />
+                        <Member
+                            image={ankit}
+                            name="Ankit Mallya"
+                            role="Legal"
+                        />
+                        <Member
+                            image={ayush}
+                            name="Ayush Kiran"
+                            role="Lead Designer"
+                        />
+                        <Member
+                            image={samarth}
+                            name="Samarth Kashyap"
+                            role="Business & Strategy"
+                        />
                     </div>
-                    <div className="col2 mobile__hidden">
-                        <div className="greyLongRectangle"></div>
+                    <div className="col3__lower">
+                        <Member
+                            image={pratheek}
+                            name="Pratheek SB"
+                            role="Core Developer"
+                        />
+                        <Member
+                            image={stuti}
+                            name="Stuti Mohanty"
+                            role="Lead App Developer"
+                        />
+                        <Member
+                            image={srujit}
+                            name="Srujit Naina"
+                            role="Lead App Developer"
+                        />
+                        <Member
+                            image={ritesh}
+                            name="Ritesh Sapata"
+                            role="Lead Web Developer"
+                        />
                     </div>
-                    <div className="col3">
+                </div>
+                <div className="bottom__col4">
+                    <div className="greyFatRectangle"></div>
+                    <div className="darkRectangle"></div>
+                    <div className="greyRectangle"></div>
+                    <div className="greyFatRectangle"></div>
+                </div>
+                {/* <div className="col3">
                         <Member
                             image={dhruv}
                             name="Dhruv Shetty"
@@ -47,37 +106,7 @@ const OurTeam = () => {
                             name="Ankit Mallya"
                             role="Legal"
                         />
-                        {/* <Member image={adithya} name="Adithya Aravind" role="Core Developer"/>
-                       <Member image={pratheek} name="Pratheek SB" role="Core Developer"/> */}
-                    </div>
-                    <div className="col4">
-                        <div className="greyFatRectangle mobile__hidden"></div>
-                    </div>
-                </div>
-                {/* <div className="bottom__lower">
-                    <div className="push">
-                        <div className="darkRectangle"></div>
-                    </div>
-                    <div className="col1">
-                        <div className="greyRectangle"></div>
-                    </div>
-                    <div className="col2">
-                        <div className="greyLongRectangle"></div>
-                    </div>
-                    <div className="col3">
-                        <Member image={ankit} name="Ankit Mallya" role="Legal"/>
-                       <Member image={rishi} name="Rishi Pachipulusu" role="Social Media & Marketing"/>
-                       <Member image={samarth} name="Samarth Kashyap" role="Business & Strategy"/>
-                    </div>
-                    <div className="col4">
-                        <div className="greyRectangle"></div>
-                        <div className="greyRectangle"></div>
-                    </div>
-                    <div className="col5">
-                        <div className="greyRectangle no-border curve-rectangle-left"></div>
-                        <div className="greyRectangle no-border curve-rectangle-right"></div>
-                    </div>
-                </div> */}
+                    </div>*/}
             </div>
         </div>
     )
