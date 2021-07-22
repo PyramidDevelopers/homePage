ed39856959bcd57125bfa6249641506c2f8d7241
Raashi Agarwal <raashiaggarwal21@gmail.com>
2021-07-22 16:15:46 +0530

    Added Showcase Slider

--- src/Components/Show/ShowBrief.js
+++ src/Components/Show/ShowBrief.js
@@ -1,112 +1,71 @@
 import React, { useEffect } from "react";
-import "./Show.css";
 import { BreifInformations } from "./Details";
+import "./Show.css";
 import nutrifamily from "../../assets/nutri-family.png";
 import nearvibe from "../../assets/near-vibe.png";
 import vitalminds from "../../assets/vital-minds.png";
 import enermanapp from "../../assets/enerman-app.png";
 import takeyourtrip from "../../assets/take-your-trip.png";
-import GridList from '@material-ui/core/GridList';
-import $ from 'jquery'
-window.jquery = window.$ = $
+import Glide from '@glidejs/glide'
+import "../../../node_modules/@glidejs/glide/dist/css/glide.core.min.css";
 
-const ShowBrief = ({onOpen}) => {
-  useEffect(() => {
-    if ($) {
-        $('#left-button').click(function (event) {
-            event.preventDefault()
-            $('.grid-list').animate(
-                {
-                    scrollLeft: '+=400',
-                },
-                1000
-            )
-        })
+const ShowBrief = ({ onOpen }) => {
+    const images = [nearvibe, vitalminds, enermanapp, takeyourtrip, nutrifamily];
+    const left = "<";
+    const right = ">";
+    const config = {
+        perView: 3,
+        // breakpoints: {
+        //     1000: {
+        //         perView: 2
+        //     },
+        //     300: {
+        //         perView: 1
+        //     }
+        // }
+    };
+    useEffect(() => {
+        new Glide('.glide', config).mount()
+    })
 
-        $('#right-button').click(function (event) {
-            event.preventDefault()
-            $('.grid-list').animate(
-                {
-                    scrollLeft: '-=400',
-                },
-                1000
-            )
-        })
-
-        $('.col').on('click', function(){
-          $('.col').removeClass('selected');
-          $(this).addClass('selected');
-      });
-        const slider = document.querySelector('.grid-list');
-        let isDown = false;
-        let startX;
-        let scrollLeft;
-        
-        slider.addEventListener('mousedown', (e) => {
-          isDown = true;
-          slider.classList.add('active');
-          startX = e.pageX - slider.offsetLeft;
-          scrollLeft = slider.scrollLeft;
-        });
-        slider.addEventListener('mouseleave', () => {
-          isDown = false;
-          slider.classList.remove('active');
-        });
-        slider.addEventListener('mouseup', () => {
-          isDown = false;
-          slider.classList.remove('active');
-        });
-        slider.addEventListener('mousemove', (e) => {
-          if(!isDown) return;
-          e.preventDefault();
-          const x = e.pageX - slider.offsetLeft;
-          const walk = (x - startX) * 1; //scroll-fast
-          slider.scrollLeft = scrollLeft - walk;
-          console.log(walk);
-        });
-    }
-  }, [$])
-
-  const images = [nearvibe, vitalminds, enermanapp, takeyourtrip, nutrifamily];
-
-  return (
-    <div className="showcase">
-      <div className="showcase-title">
-        <div className="title-text">
-          <h1>SHOW ME WHAT YOU GOT</h1>
-        </div>
-            <div className="buttons">
-              <div className="title-text1" id="right-button">
-                  <button>&lt;</button>
-              </div>
-              <div className="title-text2" id="left-button">
-                  <button>&gt;</button>
-              </div>
+    return (
+        <div className="showcase">
+            <div className="glide">
+                <div className="showcase-header">
+                    <div className="showcase-title">
+                        <h1>SHOW ME WHAT YOU GOT</h1>
+                    </div>
+                    <div className="glide__arrows buttons" data-glide-el="controls">
+                        <button className="glide__arrow glide__arrow--left navigation-arrows" data-glide-dir="<"><p className="arrow-icon">{left}</p></button>
+                        <button className="glide__arrow glide__arrow--right navigation-arrows" data-glide-dir=">"><p className="arrow-icon">{right}</p></button>
+                    </div>
+                </div>
+                <div className="glide__track showcase-slider" data-glide-el="track">
+                    <ul className="glide__slides">
+                        {BreifInformations.map(information => (
+                            <li className="glide__slide">
+                                <div className="projects-slider-tile">
+                                    <div className="col" onClick={e => onOpen(information.key)} key={information.key}>
+                                        <div className="col-text">
+                                            {information.ongoing && <div className="content">
+                                                <p>ONGOING</p>
+                                            </div>
+                                            }
+                                            <h4>{information.title}</h4>
+                                            <h5>{information.text}</h5>
+                                        </div>
+                                        <div className="col-image">
+                                            <img src={images[information.key]} alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-      </div>
-      <GridList className="grid-list">
-        <div className="main">
-          <div className="tile" >
-            {BreifInformations.map(information => (
-              <div className="col"  onClick = {e => onOpen(information.key)} key={information.key}>
-                <div className="col-text">
-                  {information.ongoing && <div className="content">
-                    <p>ONGOING</p>
-                  </div>
-                  }
-                  <h4>{information.title}</h4>
-                  <h5>{information.text}</h5>
-                </div>
-                <div className="col-image">
-                  <img src={images[information.key]} alt="" />
-                </div>
-              </div>
-            ))}
-          </div>
         </div>
-      </GridList>
-    </div>
-  );
+    );
 };
 
 export default ShowBrief;
