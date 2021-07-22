15e2bad70fc06fff34bbed1459fadadc2cff1146
imran mirza <imranmirza070901@gmail.com>
2021-07-22 12:30:29 +0530

    Done with making testimonials reponsive.(Tho, if we add more than 5 testimonials, we need to make a few changes to peek.)

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -9,18 +9,22 @@ const Testimonials = () => {
             peek: 300,
             breakpoints: {
                 2000: { peek: 250 },
-                1900: { peek: 225 },
-                1851: { peek: 175 },
-                1850: { peek: { before: 70, after: 60 } },
+                1899: { peek: 225 },
+                1850: { peek: { before: 75, after: 60 } },
                 1800: {
                     peek: { before: 40, after: 60 },
                 },
                 1780: { peek: 90 },
                 1750: { peek: { before: 55, after: 90 } },
-                1720: { peek: 60},
+                1720: { peek: 60 },
                 1680: {
-                    peek: {before:35, after:50}
-                }
+                    peek: { before: 35, after: 50 },
+                },
+                1650: { peek: { before: 50, after: 15 } },
+                400: {
+                    peek: { before: 20, after: 20 },
+                },
+                299: { peek: { before: -200, after: 10 } },
             },
         }).mount()
     })
@@ -57,6 +61,46 @@ const Testimonials = () => {
                 </div>
                 <div className="glide__track" data-glide-el="track">
                     <ul className="glide__slides testimonials-wrapper">
+                        <div className="testimonials-text">
+                            <div className="glide__slide description">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip
+                                    ex ea commodo consequat. Duis aute irure
+                                    dolor in reprehenderit in voluptate velit
+                                    esse cillum dolore eu fugiat nulla pariatur.
+                                    Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt
+                                    mollit anim id est laborum
+                                </p>
+                            </div>
+                            <div className="client">Client Name</div>
+                            <div className="company">Company Name</div>
+                        </div>
+
+                        <div className="testimonials-text">
+                            <div className="glide__slide description">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip
+                                    ex ea commodo consequat. Duis aute irure
+                                    dolor in reprehenderit in voluptate velit
+                                    esse cillum dolore eu fugiat nulla pariatur.
+                                    Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt
+                                    mollit anim id est laborum
+                                </p>
+                            </div>
+                            <div className="client">Client Name</div>
+                            <div className="company">Company Name</div>
+                        </div>
+
                         <div className="testimonials-text">
                             <div className="glide__slide description">
                                 <p>
