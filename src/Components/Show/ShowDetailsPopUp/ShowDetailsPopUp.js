1cd2f75db97a6aeefb5314432589395e11f4f36e
ammrit2312 <amriteshc101@icloud.com>
2021-08-16 20:33:43 +0530

    Added the deatils

--- src/Components/Show/ShowDetailsPopUp/ShowDetailsPopUp.js
+++ src/Components/Show/ShowDetailsPopUp/ShowDetailsPopUp.js
@@ -7,6 +7,33 @@ import vitalMinds from '../../../assets/vital-minds.png'
 import nearVibe from '../../../assets/near-vibe.png'
 import nutriFamily from '../../../assets/nutri-family.png'
 import takeYourTrip from '../../../assets/take-your-trip.png'
+import nearvibe1 from  '../../../assets/nearvibe/1.png'
+import nearvibe2 from  '../../../assets/nearvibe/2.png'
+import nearvibe3 from  '../../../assets/nearvibe/3.png'
+import nearvibe4 from  '../../../assets/nearvibe/4.png'
+import nearvibe5 from  '../../../assets/nearvibe/5.png'
+import nearvibe6 from  '../../../assets/nearvibe/6.png'
+import nearvibe7 from  '../../../assets/nearvibe/7.png'
+import vitalminds1 from '../../../assets/vitalminds/1.png'
+import vitalminds2 from '../../../assets/vitalminds/2.png'
+import vitalminds3 from '../../../assets/vitalminds/3.png'
+import vitalminds4 from '../../../assets/vitalminds/4.png'
+import vitalminds5 from '../../../assets/vitalminds/5.png'
+import enerman1 from '../../../assets/enerman/1.png'
+import enerman2 from '../../../assets/enerman/2.png'
+import enerman3 from '../../../assets/enerman/3.png'
+import takeyourtrip1 from '../../../assets/takeyourtrip/1.png'
+import takeyourtrip2 from '../../../assets/takeyourtrip/2.png'
+import trash2cash1 from '../../../assets/trashtocash/1.png'
+import trash2cash2 from '../../../assets/trashtocash/2.png'
+import trash2cash3 from '../../../assets/trashtocash/3.png'
+import trash2cash4 from '../../../assets/trashtocash/4.png'
+import trash2cash5 from '../../../assets/trashtocash/5.png'
+import khushi1 from '../../../assets/Khushi/1.png'
+import khushi2 from '../../../assets/Khushi/2.png'
+import khushi3 from '../../../assets/Khushi/3.png'
+import khushi4 from '../../../assets/Khushi/4.png'
+import khushi5 from '../../../assets/Khushi/5.png'
 
 function ShowDetailsPopUp({ index, onClose }) {
     const [demoIndex, setDemoIndex] = useState(0)
@@ -22,11 +49,12 @@ function ShowDetailsPopUp({ index, onClose }) {
     }
 
     const images = [
-        [nearVibe, nutriFamily, vitalMinds, enermanApp, takeYourTrip],
-        [vitalMinds, nutriFamily, nearVibe, enermanApp, takeYourTrip],
-        [enermanApp, nutriFamily, nearVibe, vitalMinds, takeYourTrip],
-        [takeYourTrip, nutriFamily, nearVibe, vitalMinds, enermanApp],
-        [nutriFamily, nearVibe, vitalMinds, enermanApp, takeYourTrip],
+        [nearvibe1, nearvibe2, nearvibe3, nearvibe4, nearvibe5, nearvibe6, nearvibe7],
+        [vitalMinds, vitalminds1, vitalminds2, vitalminds3, vitalminds4, vitalminds5],
+        [enermanApp, enerman1, enerman2, enerman3],
+        [takeYourTrip, takeyourtrip1, takeyourtrip2],
+        [trash2cash1, trash2cash2, trash2cash3, trash2cash4, trash2cash5],
+        [khushi1, khushi2, khushi3, khushi4, khushi5],
     ]
 
     return (
