a4fd1e27d646c19c277c8ee360f849a1a5245874
Dhruv Shetty <dks7920@nyu.edu>
2023-05-04 02:08:48 -0400

    Added scrolling when they click on a project

--- src/Components/Show/Show.js
+++ src/Components/Show/Show.js
@@ -1,4 +1,5 @@
 // import { useState } from "react";
+import { useEffect } from 'react'; 
 import ShowBrief from "./ShowBrief";
 import ShowDetailsPopUp from "./ShowDetailsPopUp/ShowDetailsPopUp";
 import "./Show.css"
@@ -21,27 +22,40 @@ const Show = () => {
             key
         })
 
-        // scroller.scrollTo("show", {
-        //     duration: 1000,
-        //     delay: 0,
-        //     smooth: "easeInOutQuart",
-        // });
+        scroller.scrollTo("showdetails", {
+            duration: 1000,
+            delay: 0,
+            smooth: "easeInOutQuart",
+            offset: -50, // Optional: Adjust this value to change the scroll position
+        });
+
     };
+    useEffect(() => {
+        if (openDetail) {
+            scroller.scrollTo("showdetails", {
+                duration: 1000,
+                delay: 0,
+                smooth: "easeInOutQuart",
+                offset: -50, // Optional: Adjust this value to change the scroll position
+            });
+        }
+    }, [openKey, openDetail]);
 
     const onClose = () => {
         // setOpenDetail(false);
         changeOpenDetail({
             type: actionTypes.CHANGE_OPEN_DETAIL
         })
-        // scroller.scrollTo("show", {
-        //     duration: 800,
-        //     delay: 0,
-        //     smooth: "easeInOutQuart",
-        // });
+        scroller.scrollTo("show", {
+            duration: 800,
+            delay: 0,
+            smooth: "easeInOutQuart",
+            offset: -50, // Optional: Adjust this value to change the scroll position
+        });
     }
 
     return (
-        <div className="show">
+        <div className="show" id="show" name="show">
             <ShowBrief onOpen = {onOpen}/>
             <div className="detailedPopup">
             {openDetail && 
