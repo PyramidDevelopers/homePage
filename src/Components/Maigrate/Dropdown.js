73b0be00bb44edd84982a24d8f0fa729c787c83c
Dhruv Shetty <dks7920@nyu.edu>
2023-06-15 17:57:58 -0400

    Updated with new component

--- src/Components/Maigrate/Dropdown.js
+++ src/Components/Maigrate/Dropdown.js
@@ -1,28 +1,51 @@
-import React, { useState } from 'react';
-import IconButton from '@material-ui/core/IconButton';
+import React, { useState, useEffect } from 'react';
+import CloseOutlined from '@material-ui/icons/CloseOutlined';
 import SearchOutlined from '@material-ui/icons/SearchOutlined';
 import './Dropdown.css';
+import { IconButton } from '@material-ui/core';
 
 const Dropdown = () => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggleOpen = () => setIsOpen(!isOpen);
-  
-    return (
-        <div >
-            {!isOpen ? 
-                <IconButton id="dropdown" onClick={toggleOpen}>
-                    <SearchOutlined />
-                </IconButton> 
-                : 
-                (
-                    <div id="content" onClick={toggleOpen}>
-                        <iframe src="https://maigrate.web.app/#/" width="400" height="500"></iframe>
-                    </div>
-                )
-            }
-        </div>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      // Check if Cmd (Mac) or Ctrl (Windows) key is pressed along with the 'K' key
+      if ((event.metaKey || event.ctrlKey) && event.key === 'k') {
+        setIsOpen((prev) => !prev);
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  const toggleOpen = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <IconButton
+        id="m-search-btn"
+        className={isOpen ? 'm-active' : ''}
+        onClick={toggleOpen}
+      >
+        {isOpen ? <CloseOutlined /> : <SearchOutlined />}
+      </IconButton>
+      <div
+        id="m-popup"
+        className={isOpen ? 'm-show' : ''}
+      >
+        {isOpen && (
+          <iframe
+            src="https://dev.d3nbl0ydl3osud.amplifyapp.com/?webbot_id=11"
+            width="400"
+            height="500"
+          ></iframe>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
