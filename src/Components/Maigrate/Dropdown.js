672ad54666515e3b4f61f5816d73650708ab65c0
Dhruv Shetty <dks7920@nyu.edu>
2023-06-16 22:09:46 -0400

    Finishing up Maigrate Integration

--- src/Components/Maigrate/Dropdown.js
+++ src/Components/Maigrate/Dropdown.js
@@ -6,10 +6,21 @@ import { IconButton } from '@material-ui/core';
 
 const Dropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      // Check if Cmd (Mac) or Ctrl (Windows) key is pressed along with the 'K' key
       if ((event.metaKey || event.ctrlKey) && event.key === 'k') {
         setIsOpen((prev) => !prev);
       }
@@ -22,6 +33,7 @@ const Dropdown = () => {
   }, []);
 
   const toggleOpen = () => setIsOpen(!isOpen);
+  const iconClass = isMobile ? 'icon-large' : '';
 
   return (
     <div>
@@ -30,19 +42,20 @@ const Dropdown = () => {
         className={isOpen ? 'm-active' : ''}
         onClick={toggleOpen}
       >
-        {isOpen ? <CloseOutlined /> : <SearchOutlined />}
-      </IconButton>
-      <div
-        id="m-popup"
-        className={isOpen ? 'm-show' : ''}
-      >
-        {isOpen && (
-          <iframe
-            src="https://dev.d3nbl0ydl3osud.amplifyapp.com/?webbot_id=11"
-            width="400"
-            height="500"
-          ></iframe>
+        {isOpen ? (
+          <CloseOutlined className={iconClass} />
+        ) : (
+          <SearchOutlined className={iconClass} />
         )}
+      </IconButton>
+      <div id="m-popup" className={isOpen ? 'm-show' : ''}>
+        <iframe
+          src="https://dev.d3nbl0ydl3osud.amplifyapp.com/?webbot_id=31"
+          width={isMobile ? "320" : "400"}
+          height={isMobile ? "600" : "700"}
+          frameBorder="0"
+          style={{ display: isOpen ? 'block' : 'none' }}
+        ></iframe>
       </div>
     </div>
   );
